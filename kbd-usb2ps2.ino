bb648531ca401027716c80878dc39818638cee21
shenyu <shenyu@shenyu.me>
2020-03-11 00:51:44 +0800

    Remove repeat flag.
    Every key need be repeated when pressed without release.

--- kbd-usb2ps2.ino
+++ kbd-usb2ps2.ino
@@ -4,21 +4,23 @@
 #include "FidPS2Host.h"
 #include "FidPS2Keyboard.h"
 
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+
 #define KEY_REPEAT_INTERVAL 360
 #define KEY_REPEAT_DELAY 8000
 
-// flag: repeat & type
-// For saving memory
-// | bin       | hex  | describe                |
-// | --------- | ---- | ----------------------- |
-// | 0000 0000 | 0x00 | No repeat & None type   |
-// | 0000 0001 | 0x01 | No repeat & Normal type |
-// | 0000 0010 | 0x02 | No repeat & E0 type     |
-// | 0000 0100 | 0x04 | No repeat & E1 type     |
-// | 0001 0001 | 0x11 | repeat & Normal type    |
-// | 0001 0010 | 0x12 | repeat & E0 type        |
-#define PS2_KEY_REPEAT_NO 0x00
-#define PS2_KEY_REPEAT_YES 0x10
+// PS/2 Key type
+// | hex  | describe    |
+// | ---- | ----------- |
+// | 0x00 | None type   |
+// | 0x01 | Normal type |
+// | 0x02 | E0 type     |
+// | 0x04 | E1 type     |
 #define PS2_KEY_TYPE_NONE 0x00
 #define PS2_KEY_TYPE_NORMAL 0x01
 #define PS2_KEY_TYPE_E0 0x02
@@ -26,12 +28,13 @@
 
 struct Ps2Key {
   uint8_t key;
-  uint8_t flag;
+  uint8_t type;
 };
 
+// HID Usage ID Max = E7 (Usage Page 07), E8-FFFF Reserved
 Ps2Key hid2ps2[0xE8];
 
-Ps2Key* last_press_key = nullptr;
+const Ps2Key* last_press_key = nullptr;
 uint16_t key_repeat_ct = 0;
 uint16_t key_repeat_delay_ct = 0;
 
@@ -44,33 +47,27 @@ uint16_t key_repeat_delay_ct = 0;
 #define HID_KEY_RIGHT_ALT 0xE6
 #define HID_KEY_RIGHT_GUI 0xE7
 
-bool Ps2KeyTypeNormal(const uint8_t& flag) {
-  return flag & 0x0F & PS2_KEY_TYPE_NORMAL;
+bool Ps2KeyTypeNormal(const uint8_t& type) {
+  return type & PS2_KEY_TYPE_NORMAL;
 }
 
-bool Ps2KeyTypeE0(const uint8_t& flag) { return flag & 0x0F & PS2_KEY_TYPE_E0; }
+bool Ps2KeyTypeE0(const uint8_t& type) { return type & PS2_KEY_TYPE_E0; }
 
-bool Ps2KeyTypeE1(const uint8_t& flag) { return flag & 0x0F & PS2_KEY_TYPE_E1; }
-
-bool Ps2KeyRepeatYes(const uint8_t& flag) {
-  return flag & 0xF0 & PS2_KEY_REPEAT_YES;
-}
+bool Ps2KeyTypeE1(const uint8_t& type) { return type & PS2_KEY_TYPE_E1; }
 
 void Ps2KeyPress(const Ps2Key& ps2_key) {
-  if (Ps2KeyTypeNormal(ps2_key.flag)) {
-    fid_ps2h_write(ps2_key.key);
+  Serial.print("Ps2 key press: ");
+  PrintHex<uint8_t>(ps2_key.key, 0x80);
+  Serial.print(", ");
+  PrintHex<uint8_t>(ps2_key.type, 0x80);
+  Serial.println("");
 
-    Serial.print("Ps2: ");
-    PrintHex<uint8_t>(ps2_key.key, 0x80);
-    Serial.println("");
-  } else if (Ps2KeyTypeE0(ps2_key.flag)) {
+  if (Ps2KeyTypeNormal(ps2_key.type)) {
+    fid_ps2h_write(ps2_key.key);
+  } else if (Ps2KeyTypeE0(ps2_key.type)) {
     fid_ps2h_write(0xE0);
     fid_ps2h_write(ps2_key.key);
-
-    Serial.print("Ps2: ");
-    PrintHex<uint8_t>(ps2_key.key, 0x80);
-    Serial.println("");
-  } else if (Ps2KeyTypeE1(ps2_key.flag)) {
+  } else if (Ps2KeyTypeE1(ps2_key.type)) {
     fid_ps2h_write(0xE1);
     fid_ps2h_write(0x14);
     fid_ps2h_write(0x77);
@@ -80,32 +77,31 @@ void Ps2KeyPress(const Ps2Key& ps2_key) {
     fid_ps2h_write(0xF0);
     fid_ps2h_write(0x77);
 
-    Serial.println("Ps2: Pause/Break");
+    Serial.println("Ps2 key press: Pause/Break");
   } else {
-    Serial.println("Ps2: not recognized");
+    Serial.println("Ps2 key press: not recognized");
   }
 }
 
 void Ps2KeyRelease(const Ps2Key& ps2_key) {
-  if (Ps2KeyTypeNormal(ps2_key.flag)) {
+  Serial.print("Ps2 key release: ");
+  PrintHex<uint8_t>(ps2_key.key, 0x80);
+  Serial.print(", ");
+  PrintHex<uint8_t>(ps2_key.type, 0x80);
+  Serial.println("");
+
+
+  if (Ps2KeyTypeNormal(ps2_key.type)) {
     fid_ps2h_write(0xF0);
     fid_ps2h_write(ps2_key.key);
-
-    Serial.print("Ps2: ");
-    PrintHex<uint8_t>(ps2_key.key, 0x80);
-    Serial.println("");
-  } else if (Ps2KeyTypeE0(ps2_key.flag)) {
+  } else if (Ps2KeyTypeE0(ps2_key.type)) {
     fid_ps2h_write(0xE0);
     fid_ps2h_write(0xF0);
     fid_ps2h_write(ps2_key.key);
-
-    Serial.print("Ps2: ");
-    PrintHex<uint8_t>(ps2_key.key, 0x80);
-    Serial.println("");
-  } else if (Ps2KeyTypeE1(ps2_key.flag)) {
-    Serial.println("Ps2: Pause/Break");
+  } else if (Ps2KeyTypeE1(ps2_key.type)) {
+    Serial.println("Ps2 key relase: Pause/Break");
   } else {
-    Serial.println("Ps2: not recognized");
+    Serial.println("Ps2 key relase: not recognized");
   }
 }
 
@@ -118,20 +114,40 @@ void InitHidKeyMapPs2() {
   // | 04  | 1C        | F0 1C      |
 
   // A-Z
-  hid2ps2[0x04] = {0x1c, PS2_KEY_REPEAT_YES & PS2_KEY_TYPE_NORMAL};
+  hid2ps2[0x04] = {0x1C, PS2_KEY_TYPE_NORMAL};
+  hid2ps2[0x05] = {0x32, PS2_KEY_TYPE_NORMAL};
+  hid2ps2[0x06] = {0x21, PS2_KEY_TYPE_NORMAL};
+
+  hid2ps2[0xE0] = {0x14, PS2_KEY_TYPE_NORMAL};  // Left Control
+  hid2ps2[0xE1] = {0x12, PS2_KEY_TYPE_NORMAL};  // Left Shift
+  hid2ps2[0xE2] = {0x11, PS2_KEY_TYPE_NORMAL};  // Left Alt
+
+  hid2ps2[0xE5] = {0x59, PS2_KEY_TYPE_NORMAL};  // Right Shift
+
+  hid2ps2[0x39] = {0x58, PS2_KEY_TYPE_NORMAL};  // Caps Lock
+
+  hid2ps2[0x53] = {0x77, PS2_KEY_TYPE_NORMAL};  // Num Lock
+
 
   // PS/2 scan code prefix with E0
   // | HID | PS/2 Make | PS/2 Break |
   // | --- | --------- | ---------- |
   // | 46  | E0 7C     | E0 F0 7C   |
-  hid2ps2[0x46] = {0x7c, PS2_KEY_REPEAT_NO & PS2_KEY_TYPE_E0};
+  hid2ps2[0x46] = {0x7c, PS2_KEY_TYPE_E0};
+
+  hid2ps2[0xE3] = {0x1F, PS2_KEY_TYPE_E0};  // Left GUI
+  hid2ps2[0xE4] = {0x14, PS2_KEY_TYPE_E0};  // Right Control
+  hid2ps2[0xE6] = {0x11, PS2_KEY_TYPE_E0};  // Right Alt
+  hid2ps2[0xE7] = {0x27, PS2_KEY_TYPE_E0};  // Right GUI
+
+
 
   // PS/2 scan code prefix with E1
   // | HID | PS/2 Make               | PS/2 Break |
   // | --- | ----------------------- | ---------- |
   // | 48  | E1 14 77 E1 F0 14 F0 77 | None       |
 
-  hid2ps2[0x48] = {0x0, PS2_KEY_REPEAT_NO & PS2_KEY_TYPE_E1};  // Pause/Break
+  hid2ps2[0x48] = {0x0, PS2_KEY_TYPE_E1};  // Pause/Break
 
   //  // A-Z
   //  k[0x04] = 0x1C;
@@ -240,21 +256,17 @@ void InitHidKeyMapPs2() {
   //  k_e0[0xE7] = 0x27;  // Right GUI
 }
 
-// Satisfy the IDE, which needs to see the include statment in the ino too.
-#ifdef dobogusinclude
-#include <spi4teensy3.h>
-#endif
-#include <SPI.h>
 
 class KbdRptParser : public KeyboardReportParser {
   void PrintKey(uint8_t mod, uint8_t key);
 
  protected:
   void OnControlKeysChanged(uint8_t before, uint8_t after);
-
   void OnKeyDown(uint8_t mod, uint8_t key);
   void OnKeyUp(uint8_t mod, uint8_t key);
   void OnKeyPressed(uint8_t key);
+  void OnKeyDown(uint8_t key);
+  void OnKeyUp(uint8_t key);
 };
 
 void KbdRptParser::PrintKey(uint8_t m, uint8_t key) {
@@ -278,21 +290,20 @@ void KbdRptParser::PrintKey(uint8_t m, uint8_t key) {
 void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key) {
   Serial.print("DN ");
   PrintKey(mod, key);
-  uint8_t c = OemToAscii(mod, key);
 
+  uint8_t c = OemToAscii(mod, key);
   if (c) {
     OnKeyPressed(c);
   }
 
-  Ps2Key& ps2_key = hid2ps2[key];
+  this->OnKeyDown(key);
+}
+
+void KbdRptParser::OnKeyDown(uint8_t key) {
+  const Ps2Key& ps2_key = hid2ps2[key];
   Ps2KeyPress(ps2_key);
 
-  if (Ps2KeyRepeatYes(ps2_key.flag)) {
-    last_press_key = &ps2_key;
-  } else {
-    last_press_key = nullptr;
-  }
-
+  last_press_key = &ps2_key;
   key_repeat_ct = 0;
   key_repeat_delay_ct = 0;
 }
@@ -304,74 +315,40 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
   MODIFIERKEYS afterMod;
   *((uint8_t*)&afterMod) = after;
 
-  Serial.print(beforeMod.bmLeftCtrl);
-  Serial.print(beforeMod.bmRightCtrl);
-  Serial.print(beforeMod.bmLeftShift);
-  Serial.print(beforeMod.bmRightShift);
-  Serial.print(beforeMod.bmLeftAlt);
-  Serial.print(beforeMod.bmRightAlt);
-  Serial.print(beforeMod.bmLeftGUI);
-  Serial.println(beforeMod.bmRightGUI);
-
-  Serial.print(afterMod.bmLeftCtrl);
-  Serial.print(afterMod.bmRightCtrl);
-  Serial.print(afterMod.bmLeftShift);
-  Serial.print(afterMod.bmRightShift);
-  Serial.print(afterMod.bmLeftAlt);
-  Serial.print(afterMod.bmRightAlt);
-  Serial.print(afterMod.bmLeftGUI);
-  Serial.println(afterMod.bmRightGUI);
-
   if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
     Serial.println("LeftCtrl changed");
 
     if (afterMod.bmLeftCtrl) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_CONTROL];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_LEFT_CONTROL);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_CONTROL];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_LEFT_CONTROL);
     }
   }
   if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
     Serial.println("LeftShift changed");
 
     if (afterMod.bmLeftShift) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_SHIFT];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_LEFT_SHIFT);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_SHIFT];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_LEFT_SHIFT);
     }
   }
   if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
     Serial.println("LeftAlt changed");
 
     if (afterMod.bmLeftAlt) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_ALT];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_LEFT_ALT);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_ALT];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_LEFT_ALT);
     }
   }
   if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
     Serial.println("LeftGUI changed");
 
     if (afterMod.bmLeftGUI) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_GUI];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_LEFT_GUI);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_LEFT_GUI];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_LEFT_GUI);
     }
   }
 
@@ -379,52 +356,36 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
     Serial.println("RightCtrl changed");
 
     if (afterMod.bmRightCtrl) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_CONTROL];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_RIGHT_CONTROL);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_CONTROL];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_RIGHT_CONTROL);
     }
   }
   if (beforeMod.bmRightShift != afterMod.bmRightShift) {
     Serial.println("RightShift changed");
 
     if (afterMod.bmRightShift) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_SHIFT];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyUp(HID_KEY_RIGHT_SHIFT);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_SHIFT];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_RIGHT_SHIFT);
     }
   }
   if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
     Serial.println("RightAlt changed");
 
     if (afterMod.bmRightAlt) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_ALT];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
-    } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_ALT];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyDown(HID_KEY_RIGHT_ALT);
+    } else { 
+      this->OnKeyUp(HID_KEY_RIGHT_ALT);
     }
   }
   if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
     Serial.println("RightGUI changed");
 
     if (afterMod.bmRightGUI) {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_GUI];
-      Ps2KeyPress(ps2_key);
-
-      last_press_key = nullptr;
+      this->OnKeyDown(HID_KEY_RIGHT_GUI);
     } else {
-      Ps2Key& ps2_key = hid2ps2[HID_KEY_RIGHT_GUI];
-      Ps2KeyRelease(ps2_key);
+      this->OnKeyUp(HID_KEY_RIGHT_GUI);
     }
   }
 
@@ -436,11 +397,15 @@ void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key) {
   Serial.print("UP ");
   PrintKey(mod, key);
 
-  Ps2Key& ps2_key = hid2ps2[key];
+  this->OnKeyUp(key);
+}
+
+void KbdRptParser::OnKeyUp(uint8_t key) {
+  const Ps2Key& ps2_key = hid2ps2[key];
   Ps2KeyRelease(ps2_key);
 
   if (last_press_key && last_press_key->key == ps2_key.key &&
-      last_press_key->flag == ps2_key.flag) {
+      last_press_key->type == ps2_key.type) {
     last_press_key = nullptr;
   }
 
@@ -482,6 +447,13 @@ void setup() {
 void loop() {
   Usb.Task();
 
+  uint8_t b;
+  while (fid_ps2h_read(&b)) {
+    Serial.print("Ps2 read: ");
+    PrintHex<uint8_t>(b, 0x80);
+    Serial.println("");
+  }
+
   if (last_press_key == nullptr) {
     return;
   }
@@ -489,7 +461,7 @@ void loop() {
   if (key_repeat_delay_ct >= KEY_REPEAT_DELAY) {
     key_repeat_ct = (key_repeat_ct + 1) % KEY_REPEAT_INTERVAL;
     if (key_repeat_ct == 0) {
-      Ps2Key& ps2_key = *last_press_key;
+      const Ps2Key& ps2_key = *last_press_key;
       Ps2KeyPress(ps2_key);
     }
   } else {
